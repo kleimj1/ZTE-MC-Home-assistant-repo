563def1be0c8e436383b6290af407cfc0a294d95
kleimj1 <jochen.kleimann@gmail.com>
2025-03-16 21:33:14 +0100

    Update __init__.py

--- custom_components/zte_router/__init__.py
+++ custom_components/zte_router/__init__.py
@@ -204,7 +204,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         _LOGGER.info("Automations already exist")
 
     return True
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up ZTE Router integration from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
 
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+
+    return True
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
